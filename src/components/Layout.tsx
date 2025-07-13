e5ee15b7889f25b774fbbe7fc934aec0242d0529
sobolllek <sobolll.ekk@gmail.com>
2025-07-13 20:17:59 +0300

    Create Layout.tsx

---
+++ src/components/Layout.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  return (
+    <div className="relative min-h-screen w-full bg-black text-white overflow-x-hidden">
+      {/* Контент */}
+      <div className="w-full max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8 py-6">
+        {children}
+      </div>
+
+      {/* Фиксированный градиент поверх, снизу */}
+      <div className="pointer-events-none fixed bottom-0 left-0 w-full h-36 bg-gradient-to-t from-black to-transparent z-40" />
+    </div>
+  );
+};
+
+export default Layout;
