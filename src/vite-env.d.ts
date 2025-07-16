b777cfd69efa6d96233b72fdcb2aec3db56f25d8
sobolllek <sobolll.ekk@gmail.com>
2025-07-16 19:55:55 +0300

    Update vite-env.d.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,23 +1,2 @@
 /// <reference types="vite/client" />
-
-interface TelegramWebApp {
-    ready(): void;
-    expand(): void;
-    close(): void;
-    setHeaderColor(color: string): void;
-    setBackgroundColor(color: string): void;
-    enableClosingConfirmation(): void;
-    disableClosingConfirmation(): void;
-    BackButton: {
-      show(): void;
-      hide(): void;
-      onClick(callback: () => void): void;
-    };
-  }
-  
-  interface Window {
-    Telegram: {
-      WebApp: TelegramWebApp;
-    };
-  }
   
