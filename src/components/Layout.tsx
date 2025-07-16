08044fe3c9964a47af8d1e950ba98c30c6213b90
sobolllek <sobolll.ekk@gmail.com>
2025-07-16 19:24:10 +0300

    Update Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,13 +2,13 @@ import React from "react";
 
 const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
-    <div className="relative min-h-screen w-full bg-black text-white overflow-x-hidden">
+    <div className="relative h-full w-full bg-black text-white overflow-x-hidden">
       {/* Контент */}
-      <div className="w-full max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8 py-6">
+      <div className="w-full max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8 py-6 h-full">
         {children}
       </div>
 
-      {/* Фиксированный градиент поверх, снизу */}
+      {/* Градиент снизу */}
       <div className="pointer-events-none fixed bottom-0 left-0 w-full h-36 bg-gradient-to-t from-black to-transparent z-40" />
     </div>
   );
