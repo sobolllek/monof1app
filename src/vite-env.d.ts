feb4087643c96e64b00fc25a3db569dd04052e74
sobolllek <sobolll.ekk@gmail.com>
2025-07-16 18:59:02 +0300

    Update vite-env.d.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,17 +1,1 @@
 /// <reference types="vite/client" />
-
-interface Window {
-    Telegram: {
-      WebApp: {
-        BackButton: {
-          show: () => void;
-          hide: () => void;
-          onClick: (callback: () => void) => void;
-          offClick: (callback: () => void) => void;
-        };
-        close: () => void;
-        expand: () => void;
-      };
-    };
-    mockBackButton: (() => void) | null;
-  }
