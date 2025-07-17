c28725c563033b16fb07051f4bea723f4850a046
sobolllek <sobolll.ekk@gmail.com>
2025-07-17 15:03:07 +0300

    Create telegram.d.ts

---
+++ src/types/telegram.d.ts
@@ -0,0 +1,16 @@
+export {};
+
+declare global {
+  interface Window {
+    Telegram?: {
+      WebApp?: {
+        BackButton: {
+          show: () => void;
+          hide: () => void;
+          onClick: (cb: () => void) => void;
+          offClick: (cb: () => void) => void;
+        };
+      };
+    };
+  }
+}
