8b97fd479dc228fb74959105ecb4a2800b64bb6d
sobolllek <sobolll.ekk@gmail.com>
2025-07-17 15:00:23 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -20,48 +20,17 @@ import LegendGarage from "./pages/LegendGarage";
 import RacerMap from "./pages/RacerMap";
 import TeamManager from "./pages/TeamManager";
 
-declare global {
-  interface Window {
-    Telegram?: {
-      WebApp: {
-        expand: () => void;
-        close: () => void;
-        enableClosingConfirmation: () => void;
-        BackButton: {
-          show: () => void;
-          hide: () => void;
-          onClick: (callback: () => void) => void;
-          offClick: (callback: () => void) => void;
-        };
-      };
-    };
-  }
-}
-
 const queryClient = new QueryClient();
 
 const App = () => {
   const [isLoading, setIsLoading] = useState(true);
-  const [isTMA, setIsTMA] = useState(false);
 
-  useEffect(() => {
-    const tgWebApp = window.Telegram?.WebApp;
-    setIsTMA(!!tgWebApp);
-
-    if (tgWebApp) {
-      tgWebApp.expand();
-      tgWebApp.enableClosingConfirmation();
-      
-      // Инициализация BackButton сразу
-      tgWebApp.BackButton.hide();
-    }
-
-    const timer = setTimeout(() => setIsLoading(false), 1000);
-    return () => clearTimeout(timer);
-  }, []);
+  const handleLoadingComplete = () => {
+    setIsLoading(false);
+  };
 
   if (isLoading) {
-    return <LoadingScreen onLoadingComplete={() => setIsLoading(false)} />;
+    return <LoadingScreen onLoadingComplete={handleLoadingComplete} />;
   }
 
   return (
@@ -70,7 +39,7 @@ const App = () => {
         <Toaster />
         <Sonner />
         <BrowserRouter>
-          <Layout isTMA={isTMA}>
+          <Layout>
             <Routes>
               <Route path="/" element={<Index />} />
               <Route path="/collection" element={<Collection />} />
