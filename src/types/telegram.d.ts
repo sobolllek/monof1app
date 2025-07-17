0c303d6eb4bac752450f977d848a0735725c7b7c
sobolllek <sobolll.ekk@gmail.com>
2025-07-17 16:25:16 +0300

    Update telegram.d.ts

--- src/types/telegram.d.ts
+++ src/types/telegram.d.ts
@@ -9,7 +9,19 @@ declare global {
           hide: () => void;
           onClick: (cb: () => void) => void;
           offClick: (cb: () => void) => void;
+          isVisible: boolean;
         };
+        MainButton: {
+          show: () => void;
+          hide: () => void;
+          setParams: (params: { 
+            is_visible?: boolean; 
+            text?: string 
+          }) => void;
+        };
+        expand: () => void;
+        enableClosingConfirmation: () => void;
+        platform: string;
       };
     };
   }
