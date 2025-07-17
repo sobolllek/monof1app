5be7f40b6b9d11b0350c28dc556ee2f28c629c3f
sobolllek <sobolll.ekk@gmail.com>
2025-07-17 16:25:01 +0300

    Update useBackButton.ts

--- src/hooks/useBackButton.ts
+++ src/hooks/useBackButton.ts
@@ -1,26 +1,31 @@
 import { useEffect } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 
-const TABBAR_ROUTES = ['/', '/collection', '/market', '/trades', '/games'];
+const NO_BACK_PATHS = ['/', '/collection', '/market', '/trades', '/games'];
 
 export function useBackButton() {
   const location = useLocation();
   const navigate = useNavigate();
+  const isTgWebApp = window.Telegram?.WebApp;
 
   useEffect(() => {
-    const isTabbarRoot = TABBAR_ROUTES.includes(location.pathname);
+    if (!isTgWebApp) return;
 
-    if (!isTabbarRoot) {
-      window.Telegram?.WebApp?.BackButton.show();
+    const shouldHideBackButton = NO_BACK_PATHS.includes(location.pathname);
+
+    if (shouldHideBackButton) {
+      window.Telegram.WebApp.BackButton.hide();
     } else {
-      window.Telegram?.WebApp?.BackButton.hide();
+      window.Telegram.WebApp.BackButton.show();
+      const handler = () => {
+        console.log('Navigating back from:', location.pathname);
+        navigate(-1);
+      };
+      window.Telegram.WebApp.BackButton.onClick(handler);
+
+      return () => {
+        window.Telegram.WebApp.BackButton.offClick(handler);
+      };
     }
-
-    const handler = () => navigate(-1);
-    window.Telegram?.WebApp?.BackButton.onClick(handler);
-
-    return () => {
-      window.Telegram?.WebApp?.BackButton.offClick(handler);
-    };
-  }, [location, navigate]);
+  }, [location, navigate, isTgWebApp]);
 }
