b5a8871415faf083cfea451fc661f1d858b1358e
sobolllek <sobolll.ekk@gmail.com>
2025-07-18 12:36:42 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoadingScreen from "./components/LoadingScreen";
 import Layout from "./components/Layout";
+import TelegramWebAppProvider from "./components/TelegramWebAppProvider";
 import Index from "./pages/Index";
 import Profile from "./pages/Profile";
 import Notifications from "./pages/Notifications";
@@ -35,23 +36,25 @@ const App = () => {
         <Toaster />
         <Sonner />
         <BrowserRouter>
-          <Layout>
-            <Routes>
-              <Route path="/" element={<Index />} />
-              <Route path="/collection" element={<Collection />} />
-              <Route path="/market" element={<Market />} />
-              <Route path="/trades" element={<Trades />} />
-              <Route path="/games" element={<Games />} />
-              <Route path="/daily-roulette" element={<DailyRoulette />} />
-              <Route path="/rating" element={<Rating />} />
-              <Route path="/profile" element={<Profile />} />
-              <Route path="/notifications" element={<Notifications />} />
-              <Route path="/legend-garage" element={<LegendGarage />} />
-              <Route path="/racer-map" element={<RacerMap />} />
-              <Route path="/team-manager" element={<TeamManager />} />
-              <Route path="*" element={<NotFound />} />
-            </Routes>
-          </Layout>
+          <TelegramWebAppProvider>
+            <Layout>
+              <Routes>
+                <Route path="/" element={<Index />} />
+                <Route path="/collection" element={<Collection />} />
+                <Route path="/market" element={<Market />} />
+                <Route path="/trades" element={<Trades />} />
+                <Route path="/games" element={<Games />} />
+                <Route path="/daily-roulette" element={<DailyRoulette />} />
+                <Route path="/rating" element={<Rating />} />
+                <Route path="/profile" element={<Profile />} />
+                <Route path="/notifications" element={<Notifications />} />
+                <Route path="/legend-garage" element={<LegendGarage />} />
+                <Route path="/racer-map" element={<RacerMap />} />
+                <Route path="/team-manager" element={<TeamManager />} />
+                <Route path="*" element={<NotFound />} />
+              </Routes>
+            </Layout>
+          </TelegramWebAppProvider>
         </BrowserRouter>
       </TooltipProvider>
     </QueryClientProvider>
