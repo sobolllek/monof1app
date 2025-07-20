b66c3db579e8c7b458d5ab9bf489834c088974a6
sobolllek <sobolll.ekk@gmail.com>
2025-07-20 23:20:50 +0300

    Update CardCarousel.tsx

--- src/components/CardCarousel.tsx
+++ src/components/CardCarousel.tsx
@@ -169,7 +169,7 @@ const CardCarousel = ({ cards, onCardClick, onCardChange }: CardCarouselProps) =
   }
 
   return (
-    <div className="w-full">
+    <div className="w-full absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
       {/* Название текущей карты (единственное место) */}
       <div className="text-center mb-4 h-6">
         <p className="text-white text-lg font-medium">
