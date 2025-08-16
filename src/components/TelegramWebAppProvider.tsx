b8d696e93defe8c5ae7edc6bc573770109d761a9
sobolllek <sobolll.ekk@gmail.com>
2025-08-16 22:08:46 +0300

    Update TelegramWebAppProvider.tsx

--- src/components/TelegramWebAppProvider.tsx
+++ src/components/TelegramWebAppProvider.tsx
@@ -2,13 +2,11 @@ import { ReactNode } from 'react';
 import useTelegramWebApp from '../hooks/useTelegramWebApp';
 
 interface TelegramWebAppProviderProps {
-  children: ReactNode;
+  children?: ReactNode;
 }
 
 const TelegramWebAppProvider = ({ children }: TelegramWebAppProviderProps) => {
-  // Инициализируем Telegram Web App хук на верхнем уровне
-  const _telegramWebApp = useTelegramWebApp();
-  
+  useTelegramWebApp();
   return <>{children}</>;
 };
 
